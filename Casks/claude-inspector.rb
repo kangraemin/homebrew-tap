1f542eebd4747bf39fbd920ac90a865355979ffb
kangraemin <remin1994@gmail.com>
2026-03-10 20:02:36 +0900

    chore: claude-inspector v1.1.2

--- Casks/claude-inspector.rb
+++ Casks/claude-inspector.rb
@@ -1,13 +1,13 @@
 cask "claude-inspector" do
-  version "1.1.1"
+  version "1.1.2"
 
   on_arm do
     url "https://github.com/kangraemin/claude-inspector/releases/download/v#{version}/Claude-Inspector-#{version}-arm64.dmg"
-    sha256 "29beec9f996683a79aa2cdba8d954de3447905e4b7c17db0da8358c96207a670"
+    sha256 "b6bdf3a08b4664488fa2233bb347642211330e709fcf2f4a77373e8f1d4766c4"
   end
   on_intel do
     url "https://github.com/kangraemin/claude-inspector/releases/download/v#{version}/Claude-Inspector-#{version}-x64.dmg"
-    sha256 "9d66cb35edeb010dd7859cc97b093572949f782e125323415ed5f4c2b86ef440"
+    sha256 "dd828802dbac6789578c4bc4a906d0b7eec2d6793e1cebc9fadc815f08919d27"
   end
 
   name "Claude Inspector"
